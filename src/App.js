93846525448a423724df62ebb2265ed2bf2574f3
DabuluSahoo <developerdabulu117@gmail.com>
2024-05-21 10:41:48 +0530

    Created a basic screen

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Messenger from "./components/Messenger"
+import LoginDialog from './components/account/LoginDialog';
+
+  function App() {
+    return (
+      <div>
+        <Messenger/>
+        <LoginDialog/>
+        </div>
+    );
+  }
+
+export default App;
---
+++ src/components/account/LoginDialog.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginDialog = () =>{
+    return <h2></h2>;
+};
+
+export default LoginDialog;
