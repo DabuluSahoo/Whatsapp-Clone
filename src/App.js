61222c4d309ea141d81bce36efbe836e320bb6b7
DabuluSahoo <developerdabulu117@gmail.com>
2024-05-21 15:00:18 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Messenger from "./components/Messenger"
-import LoginDialog from './components/account/LoginDialog';
+import React from "react";
+import Messenger from "./components/Messenger";
+import LoginDialog from "./components/account/LoginDialog";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-  function App() {
-    return (
-      <div>
-        <Messenger/>
-        <LoginDialog/>
-        </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Messenger />
+      <GoogleOAuthProvider />
+      <LoginDialog />
+    </div>
+  );
+}
 
 export default App;
--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import LoginDialog from './account/LoginDialog';
-import {AppBar, Box, Toolbar} from "@mui/material";
-import styled from '@emotion/styled';
+import React from "react";
+import LoginDialog from "./account/LoginDialog";
+import { AppBar, Box, Toolbar } from "@mui/material";
+import styled from "@emotion/styled";
 
 const Header = styled(AppBar)`
- height: 220px;
- background-color:#00bfa5;
- box-shadow: none;
+  height: 220px;
+  background-color: #00bfa5;
+  box-shadow: none;
 `;
 const Component = styled(Box)`
- height:100vh;
- background: #dcdcdc;
-`
+  height: 100vh;
+  background: #dcdcdc;
+`;
 
 const Messenger = () => {
-    return (
-       <Component>
-          <Header>
-            <Toolbar></Toolbar>
-          </Header>
-          <LoginDialog />
-        </Component>
-    
-      );
+  return (
+    <Component>
+      <Header>
+        <Toolbar></Toolbar>
+      </Header>
+      <LoginDialog />
+    </Component>
+  );
 };
 
 export default Messenger;
